5b3ae8d9fca231a98f32a216ea92647c49140c5f
changicho <changicho29@gmail.com>
2021-06-27 23:22:46 +0900

    :art: Use basic CSS
    전역에 사용할 기본 CSS 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "@/router";
 
+import "@/assets/css/basic.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <Router />
