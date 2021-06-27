08b593c8bcf8956e751440f409948efa75076d80
changicho <changicho29@gmail.com>
2021-06-27 23:23:22 +0900

    :art: Change path name
    폴더 경로 변경

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Base from "@/components/Base/Base";
+import Base from "@/components/base/Base";
 
 export const Router = () => {
   return (
